44cc1efcf6167d6c8d87a4365d5e5e2ddc372281
robin <robinvdstelt@gmail.com>
2023-10-28 10:56:03 +0200

    build website en bash script voor robin

---
+++ build_website.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e 'index\nfaq\nsoftware\nbenchmark\nabout\ncontact' | while read PAGE
+do
+  echo $PAGE
+
+  cat docs/header.html > docs/$PAGE.html
+  cat docs/$PAGE-content.html >> docs/$PAGE.html
+  cat docs/footer.html >> docs/$PAGE.html
+done
