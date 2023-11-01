0acd9c21d8493d0a11580c627fb4c4d73c745845
nick <nick@outdoorcompute.nl>
2023-11-01 13:21:55 +0100

    aoeu

--- build_website.sh
+++ build_website.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Building Webpages:"
+
 echo -e 'index\nfaq\nsoftware\nbenchmark\nabout\ncontact\norder' | while read PAGE
 do
-  echo $PAGE
+  echo "  $PAGE-content.html > docs/$PAGE.html"
 
   cat docs/header.html > docs/$PAGE.html
   cat docs/$PAGE-content.html >> docs/$PAGE.html
